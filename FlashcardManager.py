35a3d489070ed0ccebc27e4992752f37255768a0
mikolajwiduch <mikolajwiduch@gmail.com>
2024-10-05 00:24:39 +0200

    flashcard class

---
+++ FlashcardManager.py
@@ -0,0 +1,30 @@
+class FlashcardManager:
+    def __init__(self, db_manager):
+        self.db_manager = db_manager
+        self.selected_quiz_id = None
+
+    def create_quiz(self, quiz_name):
+        self.db_manager.add_quiz(quiz_name)
+
+    def select_quiz(self, quiz_name):
+        quizzes = self.db_manager.get_guizzes()
+        for quiz in quizzes:
+            if quiz[1] == quiz_name:
+                self.selected_quiz_id = quiz[0]
+                return True
+            return False
+
+    def add_flashcard(self, question, answer):
+        if self.selected_quiz_id:
+            self.db_manager.add_flashcard(question, answer, self.selected_quiz_id)
+        else:
+            raise ValueError("No quiz selected!")
+
+    def get_flashcard_for_quiz(self):
+        if self.selected_quiz_id:
+            return self.db_manager.get_flashcards(self.selected_quiz_id)
+        else:
+            raise ValueError("No quiz selected!")
+
+    def mark_flashcard_answered(self, card_id):
+        self.db_manager.mark_as_answered(card_id)
